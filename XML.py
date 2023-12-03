e9dca6cd9aa1f082fbf2a5b1dfa19cba011dd625
Jarno-de-Wit <J.deWit-5@student.tudelft.nl>
2023-12-03 03:30:19 +0100

    Fixed crash when getting tags which are nested alongside strings

--- XML.py
+++ XML.py
@@ -116,7 +116,7 @@ class XML():
         elif isinstance(item, int): #Elif the item is an integer index, return the corresponding child
             return self.database[item]
         elif item in (itm.name for itm in self.tags): #Elif the item is the name of any of the children, return the child.
-            return self.database[[itm.name for itm in self.database].index(item)] #Find the index of the first item with the same name, and return the item at that index.
+            return self.database[[itm.name for itm in self.tags].index(item)] #Find the index of the first item with the same name, and return the item at that index.
         else:
             raise KeyError(item)
 
